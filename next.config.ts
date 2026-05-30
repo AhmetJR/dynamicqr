5d9bd6e8aa5c97a7b6c3b8ddd99bc59c1cc8ae42
Bn AhmetJR <84569543+AhmetJR@users.noreply.github.com>
2026-05-31 01:45:01 +0300

    ilk yuklemes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // ESLint hatalarını derleme sırasında yoksayar
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // TypeScript hatalarını derleme sırasında yoksayar
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
